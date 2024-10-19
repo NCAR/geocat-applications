50ae3cd9b0d3222ce43c695a024f0f8ca028d8ec
Katelyn FitzGerald <7872563+kafitzgerald@users.noreply.github.com>
2024-10-18 19:55:32 -0600

    add new logos and remove old ones per NSF NCAR branding guidance

--- conf.py
+++ conf.py
@@ -257,7 +257,11 @@ html_theme_options["analytics"] = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '_static/images/logos/GeoCAT_nsf.svg'
+html_theme_options = {
+    "logo": {
+        "image_light": '_static/images/logos/NSF_NCAR_light.svg',
+        "image_dark": '_static/images/logos/NSF_NCAR_dark.svg',
+    }
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
