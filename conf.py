489b64f259d7018bf87b74ffa1facf08b8ad38b2
cyschneck <22159116+cyschneck@users.noreply.github.com>
2026-06-08 16:37:23 -0600

    ignore doi

--- conf.py
+++ conf.py
@@ -387,7 +387,6 @@ linkcheck_anchors = False
 # ignore some links that work, but are disliked by linkcheck
 # Temporary removed while Code of Conduct is updated: https://doi.org/10.5065/6w2c-a132 -> https://www.ucar.edu/who-we-are/ethics-integrity/codes-conduct/contributors
 linkcheck_ignore = [
-    r'https://doi.org/10.1080/104732299303296',
+    r'https://doi.org/*',
     r'https://www.mathworks.com/help/matlab/ref/double.sign.html',
-    r'https://doi.org/10.5065/6w2c-a132',
 ]
